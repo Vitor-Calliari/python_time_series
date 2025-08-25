4717539f28e2859b133e56bd00ae7ad19bcc8340
Vitor-Calliari <163209006+Vitor-Calliari@users.noreply.github.com>
2025-08-25 20:29:23 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,22 @@
+#Um cabeçalho de arquivo é um bloco no início do código que serve para descrever metadados sobre o arquivo.
+#Serve como um guia rápido para o desenvolvedor entender o contexto do arquivo antes de ler o código
+# ====================================================
+# Arquivo: cadeira_preto.py
+# Autor: Vitor Calliari
+# Data: 25/08/2025
+# Descrição: Arquivo criado em sala de aula para atividade.
+# ====================================================
+"""
+Uma docstring documenta módulos, classes, funções e métodos dentro do código e faz parte da documentação oficial do código.
+Pode ser utiliza em tempo de execução através do comando help().
+Documenta o código para facilitar o entendimento de outros desenvolvedores
+"""
+
+"""
+A diferença entre docstrings e o cabeçalho do arquivo é que o cabeçalho do arquivo é um comentário simples
+feito com # com informações básicas do projeto, porém não é lido pelo Python.
+Já o Docstring é uma string especial que é interpretada pelo Python e pode ser utilizada ativamente
+com comandos dentro do código.
+"""
+
 
